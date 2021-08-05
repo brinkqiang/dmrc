f4ac0e08bb850d89edbb6a699d1e0607665d449d
brinkqiang <brink.qiang@gmail.com>
2021-08-05 14:39:47 +0800

    add

--- include/dmrc.cs
+++ include/dmrc.cs
@@ -1,24 +1,4 @@
 ﻿
-// Copyright (c) 2018 brinkqiang (brink.qiang@gmail.com)
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 using System;
 using System.Collections;
 
@@ -51,12 +31,7 @@ public class CDMRC
         SetEncryptKey(key, keylen);
         SetDecryptKey(key, keylen);
     }
-    public void SetKey(string key)
-    {
-        byte[] key2 = System.Text.Encoding.ASCII.GetBytes(key);
-        SetEncryptKey(key2, key2.Length);
-        SetDecryptKey(key2, key2.Length);
-    }
+
     public void SetEncryptKey(byte[] key, int keylen)
     {
         int i, j, a;
@@ -111,6 +86,22 @@ public class CDMRC
         }
     }
 
+    public void SetEncryptKey(string key)
+    {
+        byte[] key2 = System.Text.Encoding.ASCII.GetBytes(key);
+        SetEncryptKey(key2, key2.Length);
+    }
+    public void SetDecryptKey(string key)
+    {
+        byte[] key2 = System.Text.Encoding.ASCII.GetBytes(key);
+        SetDecryptKey(key2, key2.Length);
+    }
+    public void SetKey(string key)
+    {
+        byte[] key2 = System.Text.Encoding.ASCII.GetBytes(key);
+        SetEncryptKey(key2, key2.Length);
+        SetDecryptKey(key2, key2.Length);
+    }
     static int arc4_crypt(arc4_context ctx, byte[] input, int iOffset, byte[] output, int oOffset, int length)
     {
         int x, y, a, b;
